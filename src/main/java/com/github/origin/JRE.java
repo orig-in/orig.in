054962f5ad80df7448e7a2a00af8df6d0e008986
Matthias Cullmann <matthias.cullmann@gmail.com>
2016-12-07 11:59:22 +0100

    Update JRE.java
    make it work on windows

--- src/main/java/com/github/origin/JRE.java
+++ src/main/java/com/github/origin/JRE.java
@@ -1,6 +1,7 @@
 package com.github.origin;
 
 import static java.io.File.separator;
+import static java.util.regex.Pattern.quote;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
@@ -10,7 +11,7 @@ public class JRE {
 
 	public static String jre() {
 		RuntimeMXBean mxbean = ManagementFactory.getPlatformMXBean(RuntimeMXBean.class);
-		String java = mxbean.getBootClassPath().split(separator+"lib",2)[0]+separator+"bin"+separator+"java";
+		String java = mxbean.getBootClassPath().split(quote(separator)+"lib",2)[0]+separator+"bin"+separator+"java";
 		return new File(java).exists() ? java : java + "w.exe";
 		
 	}
